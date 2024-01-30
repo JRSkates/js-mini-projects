5036bfcdaca02262fbfc83e3d2d1df232786f8b8
JRSkates <skatesproduction@gmail.com>
2024-01-30 18:13:40 +0000

    Event Listeners set up

---
+++ 3_custom_video_player/script.js
@@ -0,0 +1,18 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stopV = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stopV.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
